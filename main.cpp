000f475900256c8da37ac93132c31f4d18f17a03
Mateusz-Obirek <95253738+Mateusz-Obirek@users.noreply.github.com>
2025-01-12 15:36:11 +0100

    commit for merge conflict

--- main.cpp
+++ main.cpp
@@ -3,24 +3,25 @@ using namespace std;
 int main() {
     double waga, wzrost, BMI;
     cout<<"Witaj! Czas obliczyć Twoje BMI!!!"<<endl;
-    cout<<"Ile ważysz? (podaj w kilogramach)"<<endl;
+    cout<<"Jak gruby jesteś? (podaj w kilogramach)"<<endl;
     cin>>waga;
-    cout<<"Ile masz wzrostu? (podaj w metrach)"<<endl;
+    cout<<"Ile masz cm bydlaka? (podaj w metrach)"<<endl;
     cin>>wzrost;
     BMI=waga/(wzrost*wzrost);
     cout<<"Twoje BMI wynosi: "<<BMI<<endl;
     cout<<"Twoja diagnoza: ";
-    if (BMI<=0)
-        cout<<"Błąd. Sprawdź jeszcze raz!"<<endl;
-    if ((BMI>0)&&(BMI<16)) cout<<"wygłodzenie"<<endl;
-    if ((BMI>=16)&&(BMI<17)) cout<<"wychudzenie"<<endl;
-    if ((BMI>=17)&&(BMI<18.5)) cout<<"niedowaga"<<endl;
-    if ((BMI>=18.5)&&(BMI<25)) cout<<"pożądana masa ciała"<<endl;
-    if ((BMI>=25)&&(BMI<30)) cout<<"nadwaga"<<endl;
-    if ((BMI>=30)&&(BMI<35)) cout<<"otyłość 1 stopnia"<<endl;
-    if ((BMI>=35)&&(BMI<40)) cout<<"otyłość 2 stopnia"<<endl;
-    if (BMI>40) cout<<"otyłość 3 stopnia"<<endl;
-
+    if(!isnan(BMI)){
+        if (BMI<=0)
+            cout<<"Błąd. Sprawdź jeszcze raz!"<<endl;
+        if ((BMI>0)&&(BMI<16)) cout<<"wygłodzenie"<<endl;
+        if ((BMI>=16)&&(BMI<17)) cout<<"wychudzenie"<<endl;
+        if ((BMI>=17)&&(BMI<18.5)) cout<<"niedowaga"<<endl;
+        if ((BMI>=18.5)&&(BMI<25)) cout<<"pożądana masa ciała"<<endl;
+        if ((BMI>=25)&&(BMI<30)) cout<<"nadwaga"<<endl;
+        if ((BMI>=30)&&(BMI<35)) cout<<"gruby"<<endl;
+        if ((BMI>=35)&&(BMI<40)) cout<<"bardzo gruby"<<endl;
+        if (BMI>40) cout<<"gruby w chuj"<<endl;
+    }
 
     return 0;
 }
